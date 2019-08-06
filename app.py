54f976cb1d1f152f44351e30ceb270f586eb1fb1
Basel Shbita <basel921@gmail.com>
2019-08-06 16:36:37 -0700

    Adapters are now managed in a class && better visualization for inputs and outputs

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ import funcs
 
 app = Flask(__name__)
 
-# FORM_SEARCH_FIELD = 'srch_str'
-
 KEY_DESC = 'description'
 KEY_MODL = '__module__'
 KEY_IDENTIFIER = 'id'
 KEY_INPUTS = 'inputs'
 KEY_OUTPUTS = 'outputs'
 
-FORM_CHOSEN_ADAPTER = 'slct_adp'
+FORM_ADAPTER_SLCTD = 'slct_adp'
 FORM_ADAPTER_SUBMT = 'add_adp'
 
-class AdapterView:
+class AdapterElement:
     def __init__(self, name, module_type, identifier, description, inputs, outputs):
         self.name = name
         self.module_type = module_type
@@ -23,47 +21,56 @@ class AdapterView:
         self.inputs = inputs
         self.outputs = outputs
 
-def get_list_of_adapters():
-    adapters_list = list()
-    for a_name, a_cls in funcs.__dict__.items():
-        if isinstance(a_cls, type):
-            cls_dict = a_cls.__dict__
-            module_type = cls_dict[KEY_MODL].split('.')[-1]
-            identifier = cls_dict[KEY_IDENTIFIER]
-            description = None
-            if KEY_DESC in cls_dict:
-                description = cls_dict[KEY_DESC]
-            inputs = cls_dict[KEY_INPUTS]
-            outputs = cls_dict[KEY_OUTPUTS]
-            '''
-            inputs: Dict[str, ArgType] = {}
-            outputs: Dict[str, ArgType] = {}
-            '''
-            adapters_list.append(AdapterView(a_name, module_type, identifier, description, inputs, outputs))
-    return adapters_list
+class AdapterDB:
+    def __init__(self):
+        self.adapters = list()
+        self.initialize_adapters()
 
-def get_list_of_adapter_names(list_of_adapters):
-    adapter_names_list = list()
-    for adp_idx, adp_ins in enumerate(list_of_adapters):
-        adapter_names_list.append(f'{adp_idx} {adp_ins.module_type}/{adp_ins.identifier}.{adp_ins.name}')
-    return adapter_names_list
+    def initialize_adapters(self):
+        for a_name, a_cls in funcs.__dict__.items():
+            if isinstance(a_cls, type):
+                cls_dict = a_cls.__dict__
+                module_type = cls_dict[KEY_MODL].split('.')[-1]
+                identifier = cls_dict[KEY_IDENTIFIER]
+                description = None
+                if KEY_DESC in cls_dict:
+                    description = cls_dict[KEY_DESC]
+                inputs_dict = cls_dict[KEY_INPUTS]
+                outputs_dict = cls_dict[KEY_OUTPUTS]
+                inputs, outputs = dict(), dict()
+                for arg_name, arg_attr in inputs_dict.items():
+                    arg_attr_dict = arg_attr.__dict__
+                    inputs[arg_name] = (arg_attr_dict['id'], arg_attr_dict['val'], arg_attr_dict['optional'])
+                for arg_name, arg_attr in outputs_dict.items():
+                    arg_attr_dict = arg_attr.__dict__
+                    outputs[arg_name] = (arg_attr_dict['id'], arg_attr_dict['val'], arg_attr_dict['optional'])
+                self.adapters.append(AdapterElement(a_name, module_type, identifier, description, inputs, outputs))
+
+    def get_list_of_adapters(self):
+        return self.adapters
+
+    def get_list_of_adapter_names_for_dropdown(self):
+        adapter_names_list = list()
+        for adp_idx, adp_ins in enumerate(self.get_list_of_adapters()):
+            adapter_names_list.append(f'{adp_idx} {adp_ins.identifier}/{adp_ins.name}')
+        return adapter_names_list
 
 ################################
 
 @app.route('/pipeline', methods=['GET', 'POST'])
 def pipeline():
 
-    global g_pipeline
+    global g_pipeline, g_adapterdb
 
     # get list of adapters and their names
-    list_of_adapters = get_list_of_adapters()
-    list_of_adapter_names = get_list_of_adapter_names(list_of_adapters)
+    list_of_adapters = g_adapterdb.get_list_of_adapters()
+    list_of_adapter_names = g_adapterdb.get_list_of_adapter_names_for_dropdown()
 
     # set default option
     adp_id_str_chosen = list_of_adapter_names[0]
     # show selected adapter
-    if FORM_CHOSEN_ADAPTER in request.args:
-        adp_id_str_chosen = request.args.get(FORM_CHOSEN_ADAPTER)
+    if FORM_ADAPTER_SLCTD in request.args:
+        adp_id_str_chosen = request.args.get(FORM_ADAPTER_SLCTD)
     adp = list_of_adapters[int(adp_id_str_chosen.split(' ')[0])]
 
     # check if user submitted an adapter
@@ -89,7 +96,9 @@ def search():
 '''
 
 if __name__ == '__main__':
-    global g_pipeline
+    global g_pipeline, g_adapterdb
     g_pipeline = list()
+    g_adapterdb = AdapterDB()
+
     app.debug = True
     app.run(host='0.0.0.0', port=5000, debug=True)
