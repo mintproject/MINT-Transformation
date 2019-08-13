f96d0bf8dbc60a8d232b54b85d518bdd2e61d1ae
Basel Shbita <basel921@gmail.com>
2019-08-13 12:42:03 -0700

    added wire (graph) instance creation in the UI and support saving/loading it

--- app.py
+++ app.py
@@ -25,33 +25,80 @@ FORM_PIP_FILE_LOAD = 'pipe_config_load_file'
 FORM_PIP_FILE_SAVE = 'pipe_config_save_file'
 
 GRAPH_INST_W_REPR  = 'GraphInstanceWire'
+GRAPH_INST_ADD     = 'Create graph instance'
+GRAPH_INST_CHANGE  = '__select'
 # TODO: support NumPy
 
+# --- helpers -----------------------------------------------------------------
 
 class CustomEncoder(JSONEncoder):
+    ''' Custom encoder used serialize dictionaries and classes (i.e. AdapterElement). '''
+
     def default(self, o):
         return {'__{}__'.format(o.__class__.__name__): o.__dict__}
 
 def AdapterElement_from_json(json_obj):
+    ''' Custom decoder used to deserialize AdapterElement class. '''
+
     adp_el_str = '__AdapterElement__'
     if adp_el_str in json_obj:
         return AdapterElement.from_json(json_obj[adp_el_str])
     else:
         return json_obj
 
+def update_list_of_global_graphs_from_args_dict(args_dict):
+    ''' Update the list of g_graphs_dropdown_list based on a given arguments dictionary.
+    This method is called when loading a pipeline file externally. '''
+
+    global g_graphs_dropdown_list
+
+    for _, item in args_dict.items():
+        if item['id'] == 'graph' and item['val'] not in g_graphs_dropdown_list:
+            g_graphs_dropdown_list.append(item['val'])
+
+def init_list_of_global_graphs():
+    ''' Initialize the list of g_graphs_dropdown_list. '''
+
+    global g_graphs_dropdown_list
+
+    g_graphs_dropdown_list = ['', GRAPH_INST_ADD]
+
+def was_graph_instance_added(req_args_list):
+    ''' Check whether one of the element in a given list includes GRAPH_INST_CHANGE in name. '''
+
+    # iterate over list
+    for arg_name in req_args_list:
+        if GRAPH_INST_CHANGE in arg_name:
+            return True
+    return False
+
+# --- classes -----------------------------------------------------------------
+
 class AdapterElement:
+    ''' An adapter element includes attributes needed to contrcut a pipeline.
+    Also used for browsing the Data Transformation Adapters Catalog. '''
+
     @classmethod
     def from_json(cls, json_object):
+        ''' Create an AdapterElement class from a json_object. '''
+
         name = json_object['name']
         module_type = json_object['module_type']
         identifier = json_object['identifier']
         description = json_object['description']
         inputs = json_object['inputs']
         outputs = json_object['outputs']
+
+        # update global list of graph instances when loading from JSON file
+        update_list_of_global_graphs_from_args_dict(inputs)
+        update_list_of_global_graphs_from_args_dict(outputs)
+
         ret_class = cls(name, module_type, identifier, description, inputs, outputs)
         return ret_class
 
     def __init__(self, name, module_type, identifier, description, inputs, outputs):
+        ''' Initialize AdapterElement. '''
+
         self.name = name
         self.module_type = module_type
         self.identifier = identifier
@@ -60,28 +107,43 @@ class AdapterElement:
         self.outputs = outputs
     
     def __repr__(self):
+        ''' Print AdapterElement. '''
+
         return f'name={self.name}, module_type={self.module_type}, ' + \
             f'identifier={self.identifier}\ndescription={self.description}\n' + \
             f'inputs={self.inputs}\noutputs={self.outputs}\n\n'
 
     def get_adapter_identifier(self):
+        ''' Get AdapterElement identifier. '''
+
         return self.identifier
 
     def get_adapter_name(self):
+        ''' Get AdapterElement name. '''
+
         return self.name
 
 class AdapterDB:
+    ''' The AdapterDB is the Data Transformation Adapters Catalog.
+    It holds instances of AdapterElement class. '''
+
     def __init__(self):
+        ''' Initialize AdapterDB. '''
+
         self.adapters = list()
         self.name2object = dict()
         self.initialize_adapters()
 
     def get_adapter_object_from_name(self, adp_name):
+        ''' Get an Adapter object (class) from the name of the adapter. '''
+
         if adp_name in self.name2object:
             return self.name2object[adp_name]
         return None
 
     def initialize_adapters(self):
+        ''' Initialize adapters in AdapterDB by reading the 'funcs' python module. '''
+
         for a_name, a_cls in funcs.__dict__.items():
             if isinstance(a_cls, type):
                 cls_dict = a_cls.__dict__
@@ -104,15 +166,19 @@ class AdapterDB:
                 self.name2object[a_name] = a_cls
 
     def get_list_of_adapters(self):
+        ''' Get the list of AdapterElements in the AdapterDB. '''
+
         return self.adapters
 
     def get_list_of_adapter_names_for_dropdown(self):
+        ''' Get the list of AdapterElements names (used for dropdown in the main menu). '''
+
         adapter_names_list = list()
         for adp_idx, adp_ins in enumerate(self.get_list_of_adapters()):
             adapter_names_list.append(f'{adp_idx} {adp_ins.identifier}/{adp_ins.name}')
         return adapter_names_list
 
-################################
+# --- pipeline methods --------------------------------------------------------
 
 def get_next_index_in_g_pipeline():
     global g_pipeline
@@ -150,7 +216,7 @@ def update_g_pipeline_elements(adapter_identifier_in_pipe, input_n_output, adapt
 
 @app.route('/pipeline', methods=['GET', 'POST'])
 def pipeline():
-    global g_pipeline, g_adapterdb
+    global g_pipeline, g_adapterdb, g_graphs_dropdown_list
 
     # get list of adapters and their names
     list_of_adapters = g_adapterdb.get_list_of_adapters()
@@ -170,6 +236,8 @@ def pipeline():
         request.args[FORM_PIP_FILE_LOAD] != '': 
 
         g_pipeline.clear()
+        init_list_of_global_graphs()
+
         # load a pipeline configuration from file
         input_config_file = request.args[FORM_PIP_FILE_LOAD]
         with open(input_config_file, 'r') as read_file:
@@ -192,20 +260,29 @@ def pipeline():
         # check if user cleared the pipeline
         if FORM_PIP_CLR in request.args:
             g_pipeline.clear()
+            init_list_of_global_graphs()
 
         # check if user submitted an adapter
-        if FORM_ADPTR_SUBMT in request.args:
+        elif FORM_ADPTR_SUBMT in request.args:
             new_adapter = (get_next_index_in_g_pipeline(), deepcopy(adp))
             g_pipeline.append(new_adapter)
 
-        # check if user updated any field in pipeline
-        if FORM_PIP_UPDT in request.args:
+        # check if user updated any field in pipeline (or graph instance added)
+        elif FORM_PIP_UPDT in request.args or \
+            was_graph_instance_added(request.args):
+
             # iterate over adapter-cards
             for arg_name, arg_val in request.args.items():
                 if ('inputs' in arg_name or 'outputs' in arg_name) and arg_val != '':
+
                     input_not_output = True
                     if 'outputs' in arg_name:
                         input_not_output = False
+
+                    if GRAPH_INST_ADD == arg_val:
+                        g_graphs_dropdown_list.append(GRAPH_INST_W_REPR + str(len(g_graphs_dropdown_list) - 1))
+                        arg_val = g_graphs_dropdown_list[-1]
+
                     element_id_in_pipeline = int(arg_name.split('.')[0])
                     element_attr_in_pipeline = arg_name.split('.')[2]
                     update_g_pipeline_elements(element_id_in_pipeline, input_not_output, element_attr_in_pipeline, arg_val)
@@ -227,7 +304,7 @@ def pipeline():
 
     return render_template('pipeline.html', adp_dropdown_list=list_of_adapter_names, \
         adp_dropdown_selected_str=adp_id_str_chosen, adp_dropdown_selected_inst=adp, \
-        pipeline_adapters=g_pipeline, pipeline_exe_msg=pip_exe_msg)
+        pipeline_adapters=g_pipeline, pipeline_exe_msg=pip_exe_msg, graphs_dropdown_list=g_graphs_dropdown_list)
 
 def execute_pipeline():
     global g_pipeline, g_adapterdb
@@ -288,15 +365,20 @@ def execute_pipeline():
     except:
         raise ValueError("Pipeline cxecution failed...")
 
+# --- entrypoints -------------------------------------------------------------
+
 # Set "homepage" to index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# --- main --------------------------------------------------------------------
+
 if __name__ == '__main__':
     global g_pipeline, g_adapterdb
     g_pipeline = list()
     g_adapterdb = AdapterDB()
+    init_list_of_global_graphs()
 
     app.debug = True
     app.run(host='0.0.0.0', port=5000, debug=True)
