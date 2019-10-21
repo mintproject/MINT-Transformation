a520ae791c67c58abe9caf7f313adb79e77a9c0d
tttuan <wangxinting21@gmail.com>
2019-10-21 15:01:34 -0700

    Create pipeline via CLI
    missing mapper
    
    cliiii
    
    please review
    
    minor fix

--- dtran/main.py
+++ dtran/main.py
@@ -1,4 +1,5 @@
 import click
+
 from dtran.config_parser import ConfigParser
 
 
@@ -7,17 +8,34 @@ def cli():
     pass
 
 
-@cli.command(name="create_pipeline")
+@cli.command(name="create_pipeline", context_settings=dict(
+    ignore_unknown_options=True,
+    allow_extra_args=True,
+))
 @click.option("--config", help="full path to config")
-def create_pipeline(config):
+@click.pass_context
+def create_pipeline(ctx, config=None):
+    """
+    Creates a pipeline and execute it based on given config and input(optional).
+    To specify the input to pipeline, use (listed in ascending priority):
+    1) config file option: --config path_to_file
+    2) arg params: e.g. --FuncName.Attr=value
+    """
     parser = ConfigParser(config)
-    parsed_pipeline, parsed_inputs = parser.parse()
+    parsed_pipeline, parsed_inputs, mappings = parser.parse()
+
+    # Accept user-specified inputs: expect format of --key=value
+    for arg in ctx.args:
+        key, value = arg[2:].split("=")
+        func_name, attr_name = key.split(".")
+        if func_name in mappings:
+            parsed_inputs[mappings[func_name] + attr_name] = value
 
     # Validate parsed info
     parsed_pipeline.validate(parsed_inputs)
 
     # Execute the pipeline
-    outputs = parsed_pipeline.exec(parsed_inputs)
+    parsed_pipeline.exec(parsed_inputs)
 
 
 @cli.command(name="config_parser")
