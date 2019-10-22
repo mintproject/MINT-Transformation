ddbc5e6fe098131843a678a4b9342e26af9ac962
Basel Shbita <basel921@gmail.com>
2019-10-22 11:13:53 -0700

    update soil, cliamate

--- examples/scotts_transformations/pipeline_soil.py
+++ examples/scotts_transformations/pipeline_soil.py
@@ -3,8 +3,12 @@ from pathlib import Path
 
 from dtran import Pipeline
 from funcs import Topoflow4SoilWriteFunc
+from eval_soil_climate_defs import BARO, LOL_KURU
 
 if __name__ == "__main__":
+    import sys
+    area_str = sys.argv[1]
+    area = BARO if area_str == "baro" else LOL_KURU
     pipeline = Pipeline(
         [Topoflow4SoilWriteFunc],
         wired=[ ],
@@ -13,12 +17,12 @@ if __name__ == "__main__":
     inputs = {
         Topoflow4SoilWriteFunc.I.layer: "1",
         Topoflow4SoilWriteFunc.I.input_dir: "/ws/oct_eval_data/soilGrids/",
-        Topoflow4SoilWriteFunc.I.output_dir: "/ws/examples/scotts_transformations/tmp/soil_",
-        Topoflow4SoilWriteFunc.I.DEM_bounds: "34.221249999999, 7.362083333332, 36.446249999999, 9.503749999999",
-        Topoflow4SoilWriteFunc.I.DEM_ncols: "267",
-        Topoflow4SoilWriteFunc.I.DEM_nrows: "257",
-        Topoflow4SoilWriteFunc.I.DEM_xres_arcsecs: "30",
-        Topoflow4SoilWriteFunc.I.DEM_yres_arcsecs: "30",
+        Topoflow4SoilWriteFunc.I.output_dir: f"/ws/examples/scotts_transformations/tmp/soil_{area_str}",
+        Topoflow4SoilWriteFunc.I.DEM_bounds: area['bbox'],
+        Topoflow4SoilWriteFunc.I.DEM_ncols: area['ncols'],
+        Topoflow4SoilWriteFunc.I.DEM_nrows: area['nrows'],
+        Topoflow4SoilWriteFunc.I.DEM_xres_arcsecs: area['xres'],
+        Topoflow4SoilWriteFunc.I.DEM_yres_arcsecs: area['yres'],
     }
 
     outputs = pipeline.exec(inputs)
