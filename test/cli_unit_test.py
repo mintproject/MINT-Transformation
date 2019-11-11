10d01a1041782ce4eee44ee939aeceb932a1292a
tttuan <wangxinting21@gmail.com>
2019-11-11 14:33:32 -0800

    test cli valid args using mock

--- test/cli_unit_test.py
+++ test/cli_unit_test.py
@@ -1,54 +1,20 @@
 import pytest
 from click.testing import CliRunner
-from unittest.mock import patch, call
+from unittest.mock import patch, call, MagicMock
 
 from dtran.main import cli
 
 
-def _mock_pipeline_parser(pipeline_mock, parse_mock):
-    pipeline_mock.exec.side_effect = lambda _: True
-    mock_parsed_inputs = {
-        "keep_attr": "keep this value",
-        "substitute_attr": "substitute this value"
-    }
-
-    mock_mappings = {
-        "my_custom_func1": "keep_",
-        "my_custom_func2": "substitute_"
-    }
-
-    parse_mock.return_value = (
-        pipeline_mock,
-        mock_parsed_inputs,
-        mock_mappings
-    )
-
-    return pipeline_mock, parse_mock
-
-
 # https://click.palletsprojects.com/en/7.x/testing/
 @pytest.mark.parametrize(
-    "func_name, attr_name, arg_value, upserted_inputs",
+    "func_name, attr_name, arg_value",
     [
-        ("", "", "", {
-            "keep_attr": "keep this value",
-            "substitute_attr": "substitute this value",
-        }), ("my_custom_func2", "attr", "some value", {
-            "keep_attr": "keep this value",
-            "substitute_attr": "some value"
-        }), ("my_custom_func2", "other_attr", "some value", {
-            "keep_attr": "keep this value",
-            "substitute_attr": "substitute this value",
-            "substitute_other_attr": "some value"
-        }), ("my_custom_func4", "attr", "some value", {
-            "keep_attr": "keep this value",
-            "substitute_attr": "substitute this value",
-        }),
+        ("", "", ""),
+        ("substitute", "attr", "some value"),
     ]
 )
-@patch('dtran.config_parser.ConfigParser.parse')
-@patch('dtran.pipeline.Pipeline')
-def test_cli(pipeline_mock, parse_mock, func_name, attr_name, arg_value, upserted_inputs):
+@patch('dtran.main.ConfigParser', autospec=True)
+def test_cli_valid(parser_mock, func_name, attr_name, arg_value):
     """
     This function tests cli given 4 scenarios:
     1) User does not specify any inputs
@@ -57,17 +23,45 @@ def test_cli(pipeline_mock, parse_mock, func_name, attr_name, arg_value, upserte
     4) User trys to insert invalid inputs
     """
     # TODO: how to combine mock context and pytest fixture?
-    pipeline_mock, parse_mock = _mock_pipeline_parser(pipeline_mock, parse_mock)
+    pipeline_mock = MagicMock()
+    mock_parsed_inputs = {
+        "keep_attr": "keep this value",
+        "substitute_attr": "substitute this value"
+    }
+
+    parser_mock.return_value.parse.return_value = (
+        pipeline_mock,
+        mock_parsed_inputs
+    )
+
+    mock_user_inputs = {}
+
     runner = CliRunner()
     if not func_name or not attr_name:
         result = runner.invoke(cli, [
-            'create_pipeline', '--config', 'config/path/config.yml'
+            'create_pipeline', '--config', 'config/path'
         ])
     else:
+        mock_user_inputs = {
+            (func_name, attr_name): arg_value
+        }
         result = runner.invoke(cli, [
-            'create_pipeline', '--config', 'config/path/config.yml',
+            'create_pipeline', '--config', 'config/path',
             f'--{func_name}.{attr_name}={arg_value}',
         ])
 
     assert result.exit_code == 0
-    assert pipeline_mock.exec.mock_calls[-1] == call(upserted_inputs)
+    assert parser_mock.mock_calls == [call(mock_user_inputs), call().parse('config/path')]
+    assert pipeline_mock.exec.mock_calls[-1] == call(mock_parsed_inputs)
+
+
+@pytest.mark.parametrize(
+    "func_name, attr_name, arg_value",
+    [
+        ("", "", ""),
+        ("substitute", "attr", "some value"),
+    ]
+)
+@patch('dtran.main.ConfigParser', autospec=True)
+def test_cli_invalid(parser_mock, func_name, attr_name, arg_value):
+    pass
