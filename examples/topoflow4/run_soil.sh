7f75f5ba5801949e392ead66c29238dfa2dfd323
raunaqtri1 <raunaq.trivedi01@gmail.com>
2019-11-22 18:27:33 -0800

    add sh file for soil transformations nov-26

---
+++ examples/topoflow4/run_soil.sh
@@ -0,0 +1,15 @@
+python dtran/main.py create_pipeline --config ./examples/scotts_transformations/config_soil.yml --my_topo_soil_write_func.output_dir=/data/mint/topoflow/alwero/soilGrids
+
+python dtran/main.py create_pipeline --config ./examples/scotts_transformations/config_soil.yml --my_topo_soil_write_func.output_dir=/data/mint/topoflow/awash/soilGrids
+
+python dtran/main.py create_pipeline --config ./examples/scotts_transformations/config_soil.yml --my_topo_soil_write_func.output_dir=/data/mint/topoflow/baro/soilGrids
+
+python dtran/main.py create_pipeline --config ./examples/scotts_transformations/config_soil.yml --my_topo_soil_write_func.output_dir=/data/mint/topoflow/beko/soilGrids
+
+python dtran/main.py create_pipeline --config ./examples/scotts_transformations/config_soil.yml --my_topo_soil_write_func.output_dir=/data/mint/topoflow/ganale/soilGrids
+
+python dtran/main.py create_pipeline --config ./examples/scotts_transformations/config_soil.yml --my_topo_soil_write_func.output_dir=/data/mint/topoflow/guder/soilGrids
+
+python dtran/main.py create_pipeline --config ./examples/scotts_transformations/config_soil.yml --my_topo_soil_write_func.output_dir=/data/mint/topoflow/muger/soilGrids
+
+python dtran/main.py create_pipeline --config ./examples/scotts_transformations/config_soil.yml --my_topo_soil_write_func.output_dir=/data/mint/topoflow/shebelle/soilGrids
