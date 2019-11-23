3923299663ad56de89501a39bdea2de705114d3c
Binh Vu <binh@toan2.com>
2019-11-22 23:56:21 -0800

    update

--- examples/topoflow4/upload_soil.sh
+++ examples/topoflow4/upload_soil.sh
@@ -7,13 +7,12 @@ source ./locations.sh
 function upload_file {
     local_file=$1
     remote_file=$2
+    echo "Upload file to https://files.mint.isi.edu/remote.php/webdav/$remote_file"
     curl -X PUT -u datacatalog:$PASSWORD \
         https://files.mint.isi.edu/remote.php/webdav/$remote_file --upload-file $local_file --progress-bar | tee /dev/null
 }
 
 for name in "${!AREAS[@]}"; do
-    echo "uploading $name"
-    
     pushd /data/mint/topoflow/$name
     tar -czf isric_soil.tar.gz isric_soil
     popd
