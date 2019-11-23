09c3042b61bfc4351c07b27017414bde999f9e10
Binh Vu <binh@toan2.com>
2019-11-22 21:28:40 -0800

    update

--- examples/topoflow4/run_soil.sh
+++ examples/topoflow4/run_soil.sh
@@ -8,7 +8,7 @@ function run_transformation {
     bbox=$2
     layers="1 2 3 4 5 6 7"
 
-    if [ ! -d "/data/mint/topoflow/$area/soilGrids" ]; then
+    if [[ ! -d "/data/mint/topoflow/$area/soilGrids" ]]; then
         mkdir /data/mint/topoflow/$area/soilGrids
     fi
 
@@ -18,7 +18,7 @@ function run_transformation {
         python -m dtran.main exec_pipeline \
             --config ./topoflow_soil.yml \
             --tf_soil.output_dir=/data/mint/topoflow/$area/soilGrids \
-            --tf_soil.DEM_bounds=$bbox \
+            --tf_soil.DEM_bounds="$bbox" \
             --tf_soil.layer=$layer > /data/mint/topoflow/$area/soil.log
     done
 }
