f941a8dec1d53632b95fa53d1918e5ef4d4f6bfd
Binh Vu <binh@toan2.com>
2019-11-22 21:52:42 -0800

    update

--- examples/topoflow4/run_gpm.sh
+++ examples/topoflow4/run_gpm.sh
@@ -4,6 +4,24 @@ set -e
 
 source ./locations.sh
 
-for c in "${!AREAS[@]}"; do
-    printf "%s is in %s\n" "$c" "${AREAS[$c]}"
-done
+name=$1
+year=$2
+resolution=$3
+bbox="${AREAS[$name]}"
+
+bash ./download_gpm.sh $year /data/mint/gpm
+
+output_dir=/data/mint/topoflow/$area/gpm/$year_$resolution
+
+if [[ ! -d "$output_dir" ]]; then
+    mkdir -p "$output_dir"
+fi
+
+python -m dtran.main exec_pipeline \
+    --config ./topoflow_climate.yml \
+    --tf_climate.input_dir=/data/mint/gpm/$year \
+    --tf_climate.temp_dir=$output_dir/tmp \
+    --tf_climate.output_file=$output_dir/climate.rts \
+    --tf_climate.DEM_bounds="$bbox" \
+    --tf_climate.DEM_xres_arcsecs=$resolution \
+    --tf_climate.DEM_yres_arcsecs=$resolution > /data/mint/topoflow/$area/gpm/$year/run.log
