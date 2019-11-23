b3b2c4a100d11fe92dcb741f8583af679402091a
Binh Vu <binh@toan2.com>
2019-11-22 18:19:57 -0800

    add script downloading the data

--- examples/topoflow4/download_gpm.sh
+++ examples/topoflow4/download_gpm.sh
@@ -4,4 +4,8 @@ set -e
 YEAR=$1
 OUTPUT_DIR=$2
 
+cd $OUTPUT_DIR
 wget --user datacatalog --password $PASSWORD https://files.mint.isi.edu/remote.php/webdav/gpm_$YEAR.tar.gz
+tar -xf gpm_$YEAR.tar.gz
+rm gpm_$YEAR.tar.gz
+mv gpm_$YEAR $YEAR
