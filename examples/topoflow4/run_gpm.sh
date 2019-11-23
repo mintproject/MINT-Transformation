03ec01dc85313d88e2f90e16fe1496c19f955785
Binh Vu <binh@toan2.com>
2019-11-23 12:22:41 -0800

    update

--- examples/topoflow4/run_gpm.sh
+++ examples/topoflow4/run_gpm.sh
@@ -44,7 +44,7 @@ function exec {
 }
 
 #year=$1
-years="2009 2010 2011 2012 2013 2014 2015 2016 2017 2018"
+years="2010 2011 2012 2013 2014 2015 2016 2017 2018"
 
 for year in $years; do
     bash ./download_gpm.sh $year /data/mint/gpm
