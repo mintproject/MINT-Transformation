6d1ac4cae99797bd79c58d3ee8a561b4595f14ff
Binh Vu <binh@toan2.com>
2019-11-24 10:19:46 -0800

    update

--- examples/topoflow4/download_gpm.sh
+++ examples/topoflow4/download_gpm.sh
@@ -12,7 +12,7 @@ if [[ ! -d "$OUTPUT_DIR/$YEAR" ]]; then
     wget --user datacatalog --password $PASSWORD https://files.mint.isi.edu/remote.php/webdav/gpm_$YEAR.tar.gz
     tar -xf gpm_$YEAR.tar.gz
     rm gpm_$YEAR.tar.gz
-    mv gpm_$YEAR $YEAR
+    mv gpm* $YEAR
 else
     echo "Skip download because the files exist"
 fi
