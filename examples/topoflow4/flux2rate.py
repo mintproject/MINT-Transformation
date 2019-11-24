958a3e27fc81a2a2b62134a20c8bb8335265bc4a
Binh Vu <binh@toan2.com>
2019-11-24 10:40:09 -0800

    update

---
+++ examples/topoflow4/flux2rate.py
@@ -0,0 +1,11 @@
+import sys
+import numpy as np
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    input_path = args[0]
+    output_path = args[1]
+
+    x = np.fromfile(input_path)
+    x = x / 3600.0
+    x.tofile(output_path)
--- examples/topoflow4/run_gldas.sh
+++ examples/topoflow4/run_gldas.sh
@@ -24,7 +24,7 @@ function exec {
         --config ./topoflow_climate.yml \
         --tf_climate.input_dir=/data/mint/gldas/$year \
         --tf_climate.crop_region_dir=$output_dir/cropped_region \
-        --tf_climate.output_file=$output_dir/climate.rts \
+        --tf_climate.output_file=$output_dir/climate_flux.rts \
         --tf_climate.DEM_bounds="$bbox" \
         --tf_climate.var_name=Rainf_f_tavg \
         --tf_climate.DEM_xres_arcsecs=$resolution \
@@ -34,17 +34,33 @@ function exec {
         --config ./topoflow_climate_per_month.yml \
         --tf_climate_month.grid_dir=$output_dir/cropped_region \
         --tf_climate_month.date_regex="GLDAS_NOAH025_3H.A(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})" \
-        --tf_climate_month.output_file=$output_dir/climate.rts > $output_dir/run_climate.log
+        --tf_climate_month.output_file=$output_dir/climate_flux.rts > $output_dir/run_climate.log
 
     pushd $output_dir
+    # unit conversion
+    python flux2rate.py ./climate_flux.rts ./climate.rts
+    python flux2rate.py ./climate_flux.01.rts ./climate.01.rts
+    python flux2rate.py ./climate_flux.02.rts ./climate.02.rts
+    python flux2rate.py ./climate_flux.03.rts ./climate.03.rts
+    python flux2rate.py ./climate_flux.04.rts ./climate.04.rts
+    python flux2rate.py ./climate_flux.05.rts ./climate.05.rts
+    python flux2rate.py ./climate_flux.06.rts ./climate.06.rts
+    python flux2rate.py ./climate_flux.07.rts ./climate.07.rts
+    python flux2rate.py ./climate_flux.08.rts ./climate.08.rts
+    python flux2rate.py ./climate_flux.09.rts ./climate.09.rts
+    python flux2rate.py ./climate_flux.10.rts ./climate.10.rts
+    python flux2rate.py ./climate_flux.11.rts ./climate.11.rts
+    python flux2rate.py ./climate_flux.12.rts ./climate.12.rts
+
     # compress the file so we can delete the original file, which is much bigger
     tar -czf data.tar.gz run.log run_climate.log climate.rts climate.rti climate.*.rts cropped_region
     # remove the uncompressed files
-    rm climate.*
+    rm climate*
+
     popd
 }
 
-years="2009 2008 2010 2011 2012 2013 2014 2015 2016 2017 2018"
+years="2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018"
 #years=($1)
 
 for year in $years; do
