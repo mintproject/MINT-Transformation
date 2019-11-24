c0f9eec7a46b840afb4680d074b41ee35e6080e0
Binh Vu <binh@toan2.com>
2019-11-24 12:20:39 -0800

    update years

--- examples/topoflow4/run_gldas.sh
+++ examples/topoflow4/run_gldas.sh
@@ -61,8 +61,8 @@ function exec {
     popd
 }
 
-years="2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018"
 #years=($1)
+years="2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018"
 
 for year in $years; do
     bash ./download_gldas.sh $year /data/mint/gldas
