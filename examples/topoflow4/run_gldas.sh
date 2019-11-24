8faebea2e4ff637655a9fb0fb0eced5da7cc975f
Binh Vu <binh@toan2.com>
2019-11-24 02:22:26 -0800

    update

--- examples/topoflow4/run_gldas.sh
+++ examples/topoflow4/run_gldas.sh
@@ -33,6 +33,7 @@ function exec {
     python -m dtran.main exec_pipeline \
         --config ./topoflow_climate_per_month.yml \
         --tf_climate_month.grid_dir=$output_dir/cropped_region \
+        --tf_climate_month.date_regex="GLDAS_NOAH025_3H.A(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})" \
         --tf_climate_month.output_file=$output_dir/climate.rts > $output_dir/run_climate.log
 
     pushd $output_dir
