00c1e8b512b9390059d5afd2d4811f3d36afebba
Minh Pham <minhpham@usc.edu>
2020-01-21 03:02:58 -0800

    For deploy

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,16 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
 from flaskr.api.adapters import adapters_blueprint
 from flaskr.api.pipelines import pipelines_blueprint
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), "static"))
+print(app.static_folder)
 CORS(app)
 
 app.register_blueprint(adapters_blueprint)
 app.register_blueprint(pipelines_blueprint)
 
 
-if __name__ == '__main__':
+@app.route("/", methods=["GET"])
+@app.route("/<path:dyn_path>", methods=["GET"])
+def index(dyn_path=None):
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=10010, debug=True)
--- webapp/scripts/deploy.sh
+++ webapp/scripts/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-rm -r ../data/www/css
-rm -r ../data/www/js/*.js
-rm ../data/www/precache-manifest*
+rm -r ../flaskr/static/css
+rm -r ../flaskr/static/js/*.js
+rm ../flaskr/static/precache-manifest*
 
-cp -a build/static/css ../data/www/
-cp -a build/static/js ../data/www/
-cp -a build/index.html ../pkb/templates
-cp build/favicon.ico ../data/www/
-cp build/manifest.json ../data/www/
-cp build/service-worker.js ../data/www/
-cp build/asset-manifest.json ../data/www/
-cp build/precache-manifest* ../data/www/
+cp -a build/static/css ../flaskr/static/css
+cp -a build/static/js ../flaskr/static/js
+cp -a build/index.html ../flaskr/templates
+cp build/favicon.ico ../flaskr/static
+cp build/manifest.json ../flaskr/static
+cp build/service-worker.js ../flaskr/static
+cp build/asset-manifest.json ../flaskr/static
+cp build/precache-manifest* ../flaskr/static
