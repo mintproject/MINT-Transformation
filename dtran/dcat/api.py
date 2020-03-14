63083654c3d9a023f844d07351401a73b3814170
tttuan <wangxinting21@gmail.com>
2020-03-14 11:08:32 -0700

    not tested

--- dtran/dcat/api.py
+++ dtran/dcat/api.py
@@ -1,14 +1,24 @@
 import logging
 import time
 from datetime import datetime
+from typing import List, Dict
 
 import requests
+
+
+class StandardVariableForm:
+    def __init__(self, ontology: str, name: str, uri: str):
+        self.ontology = ontology
+        self.name = name
+        self.uri = uri
 
 
 class DCatAPI:
     instance = None
     logger = logging.getLogger("dcat_api")
     logger_api_resp = logging.getLogger("dcat_api.handle_response")
+
+    BATCH_SIZE = 200
 
     def __init__(self, dcat_url: str):
         self.dcat_url = dcat_url
@@ -64,8 +74,7 @@ class DCatAPI:
         assert resp.status_code == 200, resp.text
         return resp.json()
 
-    def update_dataset_metadata(self, dataset_id, metadata):
-        provenance_id = "b3e79dc2-8fa1-4203-ac82-b5267925191f"
+    def update_dataset_metadata(self, provenance_id, dataset_id, metadata):
         request_headers = {"Content-Type": "application/json", "X-Api-Key": self.get_api_key()}
         resp = requests.post(
             f"{self.dcat_url}/datasets/update_dataset",
@@ -79,6 +88,146 @@ class DCatAPI:
         assert resp.status_code == 200, resp.text
         parsed_response = DCatAPI.handle_api_response(resp)
         return parsed_response
+
+    def register_dataset_with_multiple_resources(
+        self,
+        provenance_id: str,
+        name: str,
+        description: str,
+        variables: List[Dict[str, str]],
+        resources_metadata: List[Dict],
+        record_id=None,
+        dataset_metadata=None,
+    ):
+        request_headers = {"Content-Type": "application/json", "X-Api-Key": self.get_api_key()}
+
+        # TODO: register a dataset
+        dataset = {
+            "provenance_id": provenance_id,
+            "name": name,
+            "description": description,
+            "metadata": dataset_metadata or {}
+        }
+        if record_id is not None:
+            dataset["record_id"] = record_id
+        resp = requests.post(
+            f"{self.dcat_url}/datasets/register_datasets",
+            headers=request_headers,
+            json={"datasets": [dataset]},
+        )
+
+        parsed_response = DCatAPI.handle_api_response(resp)
+        dataset["record_id"] = parsed_response["datasets"][0]["record_id"]
+        self.logger.debug("register dataset: %s", dataset)
+
+        # TODO: register all variables
+        resp = requests.post(
+            f"{self.dcat_url}/datasets/register_variables",
+            headers=request_headers,
+            json={
+                "variables": [
+                    {
+                        "dataset_id": dataset["record_id"],
+                        "name": var["name"],
+                        "metadata": var["metadata"],
+                        "standard_variable_ids": var["standard_name_id"],
+                    }
+                    for var in variables
+                ]
+            },
+        )
+        variables = DCatAPI.handle_api_response(resp)["variables"]
+        self.logger.debug("register variables: %s", variables)
+
+        # Register resources
+        num_resources = len(resources_metadata)
+        num_steps = num_resources // self.BATCH_SIZE + 1
+        variable_records_ids = [v["record_id"] for v in variables]
+
+        for step in range(num_steps):
+            start_idx = step * self.BATCH_SIZE
+            if (step + 1) * self.BATCH_SIZE > num_resources:
+                last_idx = num_resources
+            else:
+                last_idx = (step + 1) * self.BATCH_SIZE
+            resources = resources_metadata[start_idx:last_idx]
+
+            # TODO: register all resources
+            resource_defs = {"resources": []}
+            # Walking through each resource in dataset_obj
+            for resource in resources:
+                resource_defs["resources"].append(
+                    {
+                        "provenance_id": provenance_id,
+                        "dataset_id": dataset["record_id"],
+                        "variable_ids": variable_records_ids,
+                        "name": resource["name"],
+                        "resource_type": resource["resource_type"],
+                        "data_url": resource["data_url"],
+                        "metadata": resource["metadata"],
+                    }
+                )
+
+            # self.logger.debug(f"register resources index {start_idx} to {last_idx - 1} ")
+            print(f"register resources index {start_idx} to {last_idx - 1} ")
+
+            # ... and register them in bulk
+            resp = requests.post(
+                f"{self.dcat_url}/datasets/register_resources", headers=request_headers, json=resource_defs
+            )
+
+            parsed_response = DCatAPI.handle_api_response(resp)
+            resources = parsed_response["resources"]
+            print(f"Registered dataset_id is: {resources[0]['dataset_id']}")
+
+        return dataset, resources, variables
+
+    def register_special_variables(
+        self, variable_data: Dict
+    ):
+        if not variable_data:
+            return []
+        standard_variables = variable_data.get("standard_variables", [])
+        variable_names = variable_data.get("variable_names", [])
+        variable_metadata = variable_data.get("variable_metadata", [])
+        if not variable_names:
+            variable_names = [f"var_{idx}" for idx in range(len(variable_names))]
+        if not variable_metadata:
+            variable_metadata = [{} for idx in range(len(variable_names))]
+
+        sv_forms = []
+
+        for sv in standard_variables:
+            sv_form = StandardVariableForm(sv["ontology"], sv["name"], sv["uri"])
+            sv_forms.append(sv_form)
+
+        ids = self.register_variables(sv_forms)
+        return [{
+            "name": variable_names[idx],
+            "standard_name_id": [id],
+            "metadata": variable_metadata[idx]
+        } for idx, id in enumerate(ids)]
+
+    def register_variables(self, variable_forms: List[StandardVariableForm]) -> List[str]:
+        """
+        Register variables to the data catalog.
+        """
+        DCatAPI.logger.debug("register variables: %s", variable_forms)
+        request_headers = {"Content-Type": "application/json", "X-Api-Key": self.get_api_key()}
+        resp = requests.post(
+            f"{self.dcat_url}/knowledge_graph/register_standard_variables",
+            headers=request_headers,
+            json={"standard_variables": [var.__dict__ for var in variable_forms]},
+        )
+
+        parsed_resp = self.handle_api_response(resp)
+        ids = []
+        for var in variable_forms:
+            for record in parsed_resp["standard_variables"]:
+                if record["uri"] == var.uri:
+                    ids.append(record["record_id"])
+                    break
+        return ids
 
     def get_api_key(self):
         """
--- funcs/dcat_write_func.py
+++ funcs/dcat_write_func.py
@@ -3,6 +3,7 @@
 import subprocess
 from pathlib import Path
 from typing import Union
+import os
 
 import ujson as json
 
@@ -75,6 +76,8 @@ class DcatWriteMetadataFunc(IFunc):
         "dataset_id": "ea0e86f3-9470-4e7e-a581-df85b4a7075d"
     }
 
+    PROVENANCE_ID = "b3e79dc2-8fa1-4203-ac82-b5267925191f"
+
     def __init__(
         self, metadata: str, dataset_id: str
     ):
@@ -84,7 +87,79 @@ class DcatWriteMetadataFunc(IFunc):
 
     def exec(self) -> dict:
         response = self.dcat.update_dataset_metadata(
-            self.dataset_id, self.metadata
+            self.PROVENANCE_ID, self.dataset_id, self.metadata
+        )
+        return {"data": response}
+
+    def validate(self) -> bool:
+        return True
+
+
+class DcatBatchWriteFunc(IFunc):
+    id = "dcat_batch_write_func"
+    description = """ A batch writer adapter.
+    Register a DCAT dataset with multiple resources.
+    """
+    func_type = IFuncType.WRITER
+    inputs = {
+        "resource_json_path": ArgType.String,
+        "metadata": ArgType.String,
+        "name": ArgType.String,
+        "description": ArgType.String,
+        "resource_type": ArgType.String,
+        "variable_json_path": ArgType.String
+    }
+    outputs = {"data": ArgType.String}
+    friendly_name: str = "Data Catalog Batch Writer"
+    func_type = IFuncType.WRITER
+    example = {
+        "resource_json_path": "./uploaded.json",
+        "metadata": '[{"name": "WFP Food Prices - South Sudan", "description": "Food price dataset for South Sudan (2012-2019)"}]',
+        "name": "Test DCAT Dataset",
+        "description": "Test description for test dataset",
+        "resource_type": "zip",
+        "variable_json_path": "./variable.json"
+    }
+
+    PROVENANCE_ID = "b3e79dc2-8fa1-4203-ac82-b5267925191f"
+
+    def __init__(
+        self, resource_json_path: Union[str, Path], metadata: str, name: str,
+            description: str, resource_type: str, variable_json_path: Union[str, Path]
+    ):
+        if not os.path.exists(resource_json_path) or "json" not in str(resource_json_path) or "json" not in str(variable_json_path):
+            raise ValueError(f"Please enter a valid json file path! Your input: {resource_json_path}, {variable_json_path}")
+        with open(resource_json_path, "r") as f1, open(variable_json_path, "r") as f2:
+            self.resource_data = json.load(f1)
+            self.variable_data = json.load(f2)
+        self.metadata = json.loads(metadata)
+        self.name = name
+        self.description = description
+        self.resource_type = resource_type
+        self.dcat = DCatAPI.get_instance()
+
+    def exec(self) -> dict:
+        # register variables
+        variables = self.dcat.register_special_variables(self.variable_data)
+
+        # create resource metadata
+        resources_metadata = [
+            {
+                "data_url": file_url,
+                "name": file_name,
+                "resource_type": self.resource_type.lower(),
+                "metadata": {},
+            }
+            for file_name, file_url in enumerate(self.resource_data)
+        ]
+
+        response = self.dcat.register_dataset_with_multiple_resources(
+            self.PROVENANCE_ID,
+            self.name,
+            self.description,
+            variables,
+            resources_metadata,
+            dataset_metadata=self.metadata,
         )
         return {"data": response}
 
