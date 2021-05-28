1c04d13471b8a509af5651c9148fbe74028e1ad4
Maximiliano Osorio <maxiosorio@gmail.com>
2021-05-28 11:09:10 -0400

    workaround change directory hack

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-
+#workaround change directory hack
+full_config_path=$(realpath "$@"))
 cd /ws
 source /root/.bashrc
-dotenv -f .env.docker run python -m dtran.main exec_pipeline --config "$@"
+dotenv -f .env.docker run python -m dtran.main exec_pipeline --config ${full_config_path}
